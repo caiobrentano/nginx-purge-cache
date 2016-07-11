a9fed00e3e3fa70507347360ca404d0de57d7833
caio.passos <caio.passos@corp.globo.com>
2016-07-11 14:17:45 -0300

    Some tests for bin/purger.py

--- bin/purger.py
+++ bin/purger.py
@@ -1,36 +1,49 @@
 import json
 import socket
+import subprocess
 import urllib
 
-from urllib.parse import urlparse
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    # py2
+    from urlparse import urlparse
 
 import settings
 
 def create_purge_command(url, cmd_template):
     parsed = urlparse(url)
+    delete_cmd = None
 
     if parsed.netloc:
         delete_cmd = cmd_template.format(path=parsed.path)
 
     return delete_cmd
 
-def purge(hostname, url):
+def purge(hostname):
 
-    response = urllib.urlopen(url)
+    get_url = '{url}/hosts/pending_purge?hostname={hostname}'.format(
+        url=settings.API_URL, hostname=hostname)
+
+    notify_url = '{url}/purge'.format(url=settings.API_URL)
+
+    response = urllib.urlopen(get_url)
     urls = json.loads(response.read())
 
     for url in urls:
         purge_cmd = create_purge_command(url, settings.DELETE_TMPLT)
 
         if purge_cmd:
-            pass
             # execute delete
-            # cmd_output = subprocess.check_output(purge_cmd, shell=True)
+            cmd_output = subprocess.check_output(purge_cmd, shell=True)
+
+            params = urllib.urlencode({
+                'url': url,
+                'hostname': hostname,
+                'command_output': cmd_output
+            })
+
+            _ = urllib.urlopen(notify_url, params)
 
 if __name__ == 'main':
-    hostname = socket.gethostname()
-
-    url = '{url}/hosts/pending_purge?hostname={hostname}'.format(
-        url=settings.API_URL, hostname=hostname)
-
-    purge(hostname, url)
+    purge(socket.gethostname())
--- tests/test_purge_urls.py
+++ tests/test_purge_urls.py
@@ -1,9 +1,24 @@
 import unittest
 
+from bin.purger import create_purge_command
+
 class PurgeUrlsTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.cmd_template = 'random {path} template'
 
-    def tearDown(self):
-        pass
+    def test_create_purge_command_valid_url(self):
+
+        cmd = create_purge_command('http://domain/path/to/purge', self.cmd_template)
+        expected = self.cmd_template.format(path='/path/to/purge')
+
+        self.assertEqual(cmd, expected)
+
+    def test_create_purge_command_invalid_url(self):
+
+        cmd = create_purge_command('domain / path / t o / purge', self.cmd_template)
+        self.assertIsNone(cmd)
+
+
+if __name__ == '__main__':
+    unittest.main()
