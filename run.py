9d48dc2e9ae9ddc4f81d7f9aee9e9a39f98332fa
caio.passos <caio.passos@corp.globo.com>
2016-07-13 16:18:11 -0300

    run port fixed

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ import os
 from api.app import app
 
 debug = True if os.environ.get('DEBUG', False) == 'True' else False
-app.run(debug=debug)
+port = os.environ.get('PORT', 5000)
+app.run(debug=debug, port=port)
