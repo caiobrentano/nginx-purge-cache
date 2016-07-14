dcf60fe4daceb52a2d97c9d6688d8c9d87735e29
caio.passos <caio.passos@corp.globo.com>
2016-07-14 11:09:38 -0300

    Host set on app.run()

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os
 
 from api.app import app
 
+host = os.environ.get('HOST', '0.0.0.0')
 debug = True if os.environ.get('DEBUG', False) == 'True' else False
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=debug, port=port)
+
+app.run(host=host, debug=debug, port=port)
